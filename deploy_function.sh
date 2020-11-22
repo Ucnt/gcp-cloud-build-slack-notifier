bda5eae75be4660da947067e334e741dbf619253
Matt Svensson <matt.svensson@gmail.com>
2020-11-22 09:17:22 -0500

    make adjustable based on num arguments

--- deploy_function.sh
+++ deploy_function.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-gcloud functions deploy cloud-build-notifier \
-  --project=$1 \
-  --memory=256MB \
-  --runtime=python37 \
-  --trigger-topic=cloud-builds \
-  --source=./source \
-  --entry-point=run_notifier \
-  --service-account=$2
+if [ $# -eq 1 ] 
+then
+    gcloud functions deploy cloud-build-notifier \
+      --project=$1 \
+      --memory=256MB \
+      --runtime=python37 \
+      --trigger-topic=cloud-builds \
+      --source=./source \
+      --entry-point=run_notifier 
+elif [ $# -eq 2 ] 
+then
+    gcloud functions deploy cloud-build-notifier \
+      --project=$1 \
+      --memory=256MB \
+      --runtime=python37 \
+      --trigger-topic=cloud-builds \
+      --source=./source \
+      --entry-point=run_notifier \
+      --service-account=$2
+else
+    echo "Incorrect arguments given. See Documentation."
+fi
+
+
+
+
